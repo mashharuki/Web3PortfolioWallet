7a64c853c42775d4d1cef3f8ddf55e3348ae3648
mashharuki <avp.104.106.107@ae.auone-net.jp>
2023-04-15 20:53:56 +0900

    convert from js to ts

--- packages/frontend/src/components/hooks/UseContract.ts
+++ packages/frontend/src/components/hooks/UseContract.ts
@@ -3,9 +3,9 @@
  */
 import BloctoSDK from '@blocto/sdk';
 import Web3 from 'web3';
-import DNS from './../../contracts/DNS.json';
-import MyToken from './../../contracts/MyToken.json';
-import { CHAIN_ID, DNS_ADDRESS, MYTOKEN_ADDRESS, RPC_URL } from "./../common/Constant";
+import DNS from '../../contracts/DNS.json';
+import MyToken from '../../contracts/MyToken.json';
+import { CHAIN_ID, DNS_ADDRESS, MYTOKEN_ADDRESS, RPC_URL } from "../common/Constant";
 
 /**
  * getProvider メソッド
@@ -21,7 +21,7 @@ export const getProvider = () => {
  * @param contractAbi コントラクトABI
  * @param contractAddress コントラクトアドレス
  */
-const createContractObject = ( contractAbi, contractAddress) => {
+const createContractObject = ( contractAbi:any, contractAddress: string) => {
       // get provider
       const provider = getProvider();
       // create Contract Object
@@ -41,7 +41,7 @@ export const connectWallet = async() => {
             }
       });
       // request
-      const signers = await bloctoSDK.ethereum.request({ method: 'eth_requestAccounts' });
+      const signers = await bloctoSDK.ethereum?.request({ method: 'eth_requestAccounts' });
       const signer = signers[0]
 
       return {
@@ -54,11 +54,11 @@ export const connectWallet = async() => {
  * getDid method
  * @param signer ログイン中のsignerオブジェクト
  */
-export const getDid = async(signer) => {
+export const getDid = async(signer:any) => {
       // call createContractObject
-      const FactoryContract = createContractObject(DNS.abi, DNS_ADDRESS);
+      const DNSContract = createContractObject(DNS.abi, DNS_ADDRESS);
       // get did info
-      const result = await FactoryContract.methods.dids(signer).call();
+      const result = await DNSContract.methods.dids(signer).call();
       return result;
 };
 
@@ -66,7 +66,7 @@ export const getDid = async(signer) => {
  * getTokenBalanceOf メソッド
  * @param signer ログイン中のsignerオブジェクト
  */
-export const getTokenBalanceOf = async(signer) => {
+export const getTokenBalanceOf = async(signer:any) => {
       // call createContractObject メソッド
       const MyTokenContract = await createContractObject(MyToken.abi, MYTOKEN_ADDRESS);
       // get token balance
@@ -78,11 +78,11 @@ export const getTokenBalanceOf = async(signer) => {
  * getRegisterStatusメソッド
  * @param signer ログイン中のsignerオブジェクト
  */
-export const getRegisterStatus = async(signer) => {
+export const getRegisterStatus = async(signer:any) => {
       // call createContractObject メソッド
-      const FactoryContract = createContractObject(DNS.abi, DNS_ADDRESS);
+      const DNSContract = createContractObject(DNS.abi, DNS_ADDRESS);
       // get status info
-      var status = await FactoryContract.methods.isRegistered(signer).call();
+      var status = await DNSContract.methods.isRegistered(signer).call();
       return status;
 };
 
@@ -90,11 +90,11 @@ export const getRegisterStatus = async(signer) => {
  * getVcsメソッド
  * @param did ログイン中のdid情報
  */
-export const getVcs = async(did) => {
+export const getVcs = async(did:any) => {
       // call createContractObject メソッド
-      const FactoryContract = createContractObject(DNS.abi, DNS_ADDRESS);
+      const DNSContract = createContractObject(DNS.abi, DNS_ADDRESS);
       // get Verifiable Credentials info
-      var vcs = await FactoryContract.methods.getVcs(did).call();
+      var vcs = await DNSContract.methods.getVcs(did).call();
       return vcs;
 };
 
